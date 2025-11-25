a7078748108e68b48c204aea4aacc3bce3cac7fe
Sinan Pmk <sinan@finnest.io>
2025-11-25 09:35:02 +0530

    serve static

--- server/app.js
+++ server/app.js
@@ -12,6 +12,12 @@ dotenv.config();
 
 const app = express();
 
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
